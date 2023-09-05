32a3f254bef228c1e18092c86f42517f3c8f8784
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2023-09-05 13:29:29 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -221,8 +221,7 @@ with container:
     
     if submit_button and user_input:
        output = conversational_chat(user_input)
-       utc_now = datetime.utcnow()
-   
+       
        with response_container:
            for i, (query, answer) in enumerate(st.session_state.chat_history):
                message(query, is_user=True, key=f"{i}_user", avatar_style="big-smile")
